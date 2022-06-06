0da635902e711c17dc52e5e6dbe1f8f3a37cf40f
dusiko <zheka_yura@ukr.net>
2022-06-06 20:31:38 +0300

    1 less

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header/Header'
+import Goods from './Goods';
+
+const siteData = {
+	site_name: 'my test site name',
+	nav: [
+		{
+			'link': 'nav1',
+			'text': 'my link'
+		},
+		{
+			'link': 'nav2',
+			'text': 'my link 2'
+		},
+		{
+			'link': 'nav3',
+			'text': 'my link 3'
+		}
+	]
+
+}
+
+const goods = [{
+	'title': 'apple',
+	'cost': 330,
+	'image': 'https://cdn2.iconfinder.com/data/icons/gardening-caramel-vol-1/256/APPLE-256.png'
+
+},
+{
+	'title': 'pear',
+	'cost': 530,
+	'image': 'https://cdn1.iconfinder.com/data/icons/kitchen-and-food-2/44/pear-256.png'
+
+},
+{
+	'title': 'berry',
+	'cost': 240,
+	'image': 'https://cdn2.iconfinder.com/data/icons/fruit-and-vegetable-64/340/fruit_vegan_berry_cherry_sour_organic_food-256.png'
+
+},
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Header data={siteData}></Header>
+			{goods.map(item => <Goods key={item.title} title={item.title} cost={item.cost} image={item.image} />)}
+			<Goods></Goods>
+		</>
+	);
 }
 
 export default App;
---
+++ src/Test2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Test2 extends React.Component {
+	render() {
+		return (
+			<h2>Test2 component 2</h2>
+		)
+	}
+}
+
+export default Test2;
